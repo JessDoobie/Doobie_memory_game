d03c28c245eaf87021b3cb73877f071fe6e9995a
JessDoobie <mysticdaze420@gmail.com>
2026-01-23 00:59:40 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import time, random, string
 from flask import Flask, render_template, request, jsonify
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder="static")
+
 import os
 import random
 import string
